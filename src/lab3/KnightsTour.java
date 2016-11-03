c833b96de351843c35884beb49138d2df8bcc0c4
Greg Fletcher <gnfletcher@gmail.com>
2016-11-02 22:02:26 -0400

    No real change.

--- src/lab3/KnightsTour.java
+++ src/lab3/KnightsTour.java
@@ -10,89 +10,117 @@ import java.util.ArrayList;
  *
  */
 public class KnightsTour {
-    public ArrayList<OrderedPair> board;
-    public ArrayList<OrderedPair> usedMoves;
-    public int boardSize;
-    public int path;
+  public ArrayList<OrderedPair> board;
+  public ArrayList<OrderedPair> usedMoves;
+  public int boardSize;
+  public int path;
 
-    public KnightsTour(int boardSize, int x, int y) {
-        this.boardSize = boardSize;
-        createBoard();
-        usedMoves = new ArrayList<OrderedPair>();
-        path = 0;
-        OrderedPair first = new OrderedPair(x, y);
-        if (move(first)) {
-            printMoves();
-        } else {
-            System.out.println("No Solution Found!");
-        }
+  public KnightsTour(int boardSize, int x, int y) {
+    this.boardSize = boardSize;
+    createBoard();
+    usedMoves = new ArrayList<OrderedPair>();
+    path = 0;
+    OrderedPair first = new OrderedPair(x, y);
+    if (move(first)) {
+      printMoves();
+    } else {
+      System.out.println("No Solution Found!");
     }
+  }
 
-    public boolean move(OrderedPair current) {
-        
-board.remove(current);
-        usedMoves.add(current);
-        if (board.isEmpty()) {
-            return true;
-        }
-        if (board.contains(new OrderedPair(current.getX() + 1, current.getY() + 2)) && move(new OrderedPair(current.getX() + 1, current.getY() + 2))) {
-            return true;
-        } else if (board.contains(new OrderedPair(current.getX() + 2, current.getY() + 1)) && move(new OrderedPair(current.getX() + 2, current.getY() + 1))) { 
-            return true;
-        }else if (board.contains(new OrderedPair(current.getX() + 1, current.getY() - 2)) && move(new OrderedPair(current.getX() + 1, current.getY() - 2))) {
-            return true;
-        }else if (board.contains(new OrderedPair(current.getX() + 2, current.getY() - 1)) && move(new OrderedPair(current.getX() + 2, current.getY() - 1))) {
-            return true;
-        } else if (board.contains(new OrderedPair(current.getX() - 1, current.getY() + 2)) && move(new OrderedPair(current.getX() - 1, current.getY() + 2))) {
-            return true;
-        }else if (board.contains(new OrderedPair(current.getX() - 2, current.getY() + 1)) && move(new OrderedPair(current.getX() - 2, current.getY() + 1))) {
-            return true;
-        }else if (board.contains(new OrderedPair(current.getX() - 1, current.getY() - 2)) && move(new OrderedPair(current.getX() - 1, current.getY() - 2))) {
-            return true;
-        }else if (board.contains(new OrderedPair(current.getX() - 2, current.getY() - 1)) && move(new OrderedPair(current.getX() - 2, current.getY() - 1))) {
+  public boolean move(OrderedPair current) {
 
-            return true;
-        }
-        board.add(current);
-        usedMoves.remove(current);
-        return false;
+    board.remove(current);
+    usedMoves.add(current);
+    //printBoard();
+    if (board.isEmpty()) {
+      return true;
     }
-
-    public void createBoard() {
-        if (boardSize <= 0) {
-            System.out.print("Invalid board size.");
-        } else {
-            board = new ArrayList<OrderedPair>();
-            for (int i = 0; i < boardSize; i++) {
-                for (int j = 0; j < boardSize; j++) {
-                    board.add(new OrderedPair(i, j));
-                }
-            }
-        }
+    //System.out.println(current.getX()+ ","+current.getY());
+    OrderedPair testPair = new OrderedPair(current.getX()+1, current.getY() + 2);
+    if (board.contains(testPair) && move(testPair)) {
+      return true;
     }
-
-    public void printMoves() {
-        int exp = 1;
-        int size = boardSize * boardSize;
-        while (size / 10 > 0) {
-            size /= 10;
-            exp++;
-        }
-        System.out.println("Solution Found!");
-        int[][] board = new int[usedMoves.size()][usedMoves.size()];
-        int counter = 1;
-        for (OrderedPair move : usedMoves) {
-            if (board[move.getX()][move.getY()] == 0) {
-                board[move.getX()][move.getY()] = counter;
-                counter++;
-            }
-        }
-        for (int i = 0; i < boardSize; i++) {
-            for (int j = 0; j < boardSize; j++) {
-                System.out.printf("%" + exp + "d", board[j][i]);
-                System.out.print(" | ");
-            }
-            System.out.println("");
-        }
+    testPair = new OrderedPair(current.getX() + 2, current.getY() + 1);
+    if (board.contains(testPair) && move(testPair)) {
+      return true;
     }
+    testPair = new OrderedPair(current.getX() + 1, current.getY() - 2);
+    if (board.contains(testPair) && move(testPair)) {
+      return true;
+    }
+    testPair = new OrderedPair(current.getX() + 2, current.getY() - 1);
+    if (board.contains(testPair) && move(testPair)) {
+      return true;
+    }
+    testPair = new OrderedPair(current.getX() - 1, current.getY() + 2);
+    if (board.contains(testPair) && move(testPair)) {
+      return true;
+    }
+    testPair = new OrderedPair(current.getX() - 2, current.getY() + 1);
+    if (board.contains(testPair) && move(testPair)) {
+      return true;
+    }
+    testPair = new OrderedPair(current.getX() - 1, current.getY() - 2);
+    if (board.contains(testPair) && move(testPair)) {
+      return true;
+    }
+    testPair = new OrderedPair(current.getX() - 2, current.getY() - 1);
+    if (board.contains(testPair) && move(testPair)) {
+      return true;
+    }
+    board.add(current);
+    usedMoves.remove(current);
+    //printBoard();
+    return false;
+  }
+  
+  public void printBoard(){
+    for(OrderedPair pair : board){
+      System.out.print("(" + pair.getX() + "," + pair.getY() + ")");
+    }
+    System.out.println("");
+    for(OrderedPair pair : usedMoves){
+      System.out.print("(" + pair.getX() + "," + pair.getY() + ")");
+    }
+    System.out.println("");
+  }
+
+  public void createBoard() {
+    if (boardSize <= 0) {
+      System.out.print("Invalid board size.");
+    } else {
+      board = new ArrayList<OrderedPair>();
+      for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
+          board.add(new OrderedPair(i, j));
+        }
+      }
+    }
+  }
+
+  public void printMoves() {
+    int exp = 1;
+    int size = boardSize * boardSize;
+    while (size / 10 > 0) {
+      size /= 10;
+      exp++;
+    }
+    System.out.println("Solution Found!");
+    int[][] board = new int[usedMoves.size()][usedMoves.size()];
+    int counter = 1;
+    for (OrderedPair move : usedMoves) {
+      if (board[move.getX()][move.getY()] == 0) {
+        board[move.getX()][move.getY()] = counter;
+        counter++;
+      }
+    }
+    for (int i = 0; i < boardSize; i++) {
+      for (int j = 0; j < boardSize; j++) {
+        System.out.printf("%" + exp + "d", board[j][i]);
+        System.out.print(" | ");
+      }
+      System.out.println("");
+    }
+  }
 }
--- src/lab3/driver.java
+++ src/lab3/driver.java
@@ -17,7 +17,7 @@ public class driver {
     String in = "(2+2)(3+4)4";
     System.out.println(String.valueOf(Postfix.evaluate(post)));
     System.out.println(String.valueOf(Infix.evaluate(in)));
-    new KnightsTour(8, 0, 0);
+    new KnightsTour(9, 3, 3);
   }
 
 }
